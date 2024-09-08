ddd03a5f71fd262bc6a95737433e1d205e736046
kardelenssu <kurtkardelensu@gmail.com>
2024-09-08 16:40:08 +0200

    Test Case 4.1, 4.2, 4.3 ve 4.4 yapildi.

--- src/Tests/TC_0402.java
+++ src/Tests/TC_0402.java
@@ -1,4 +1,42 @@
 package Tests;
 
-public class TC_0402 {
+import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class TC_0402 extends BaseDriver {
+    @Test
+
+    public void test1(){
+
+
+        driver.get("https://www.akakce.com/");
+        MyFunc.wait(3);
+
+        WebElement girisYap = driver.findElement(By.xpath("//div[@id= 'H_rl_v8']/a[2]"));
+        girisYap.click();
+        MyFunc.wait(3);
+
+        WebElement emailBilgisi = driver.findElement(By.xpath("(//form[@action='/akakcem/nojs/']//input)[6]"));
+        emailBilgisi.sendKeys("alikilinc.gl@gmail.com");
+        MyFunc.wait(3);
+
+        WebElement sifreBilgisi = driver.findElement(By.xpath("(//form[@action='/akakcem/nojs/']//input)[7]"));
+        sifreBilgisi.sendKeys("YanlisSifre");
+        MyFunc.wait(3);
+
+
+        WebElement girisYapButonu = driver.findElement(By.xpath("(//form[@action='/akakcem/nojs/']//input)[9]"));
+        girisYapButonu.click();
+        MyFunc.wait(3);
+
+        WebElement tamamButonu = driver.findElement(By.xpath("//div[@class='buttons']/button[1]"));
+        tamamButonu.click();
+        MyFunc.wait(3);
+
+        driver.quit();
+
+    }
 }
