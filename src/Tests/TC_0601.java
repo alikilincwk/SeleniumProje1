55fe073eef79e24b2445a2c883165a68b0978053
Ali <alikilinc.wk@gmail.com>
2024-09-08 15:18:51 +0300

    TC_0601 tamamlandı.

--- src/Tests/TC_0601.java
+++ src/Tests/TC_0601.java
@@ -1,11 +1,39 @@
 package Tests;
+
+import Utility.MyFunc;
+import org.junit.Assert;
 import org.junit.Test;
 import Utility.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 
 public class TC_0601 extends BaseDriver {
     @Test
     public void TC0601() {
+        driver.get("https://www.akakce.com/");
+        driver.findElement(By.xpath("//div[@id='H_rl_v8']//a[text()='Giriş Yap']")).click();
+        MyFunc.wait(1);
+        driver.findElement(By.xpath("//input[@id='life']")).sendKeys("alikilinc.gl@gmail.com");
+        MyFunc.wait(1);
+        driver.findElement(By.xpath("//input[@id='lifp']")).sendKeys("Testtest123");
+        MyFunc.wait(1);
+        driver.findElement(By.xpath("//input[@id='lfb']")).click();
+        MyFunc.wait(1);
+        driver.findElement(By.xpath("//a[@id='H_a_v8']")).click();
+        MyFunc.wait(1);
+        driver.findElement(By.xpath("//a[text()='Hesabım']")).click();
+        MyFunc.wait(1);
+        driver.findElement(By.xpath("//a[text()='Mesajlarım']")).click();
+        MyFunc.wait(1);
 
+        try {
+            Assert.assertTrue("Mesaj bulunamadı yazısı doğrulanamadı.", driver.findElement(By.xpath("//p[text()='Listelenecek mesaj bulunamadı.']")).getText().equals("Listelenecek mesaj bulunamadı."));
+        }catch (NoSuchElementException e){
+            MyFunc.wait(1);
+        }
+        MyFunc.wait(1);
+
+        tearDown();
 
     }
 }
